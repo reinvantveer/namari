233b30888a28116f6df5c10a5ce36e1ba6c2541d
reinvantveer <reinvantveer@gmail.com>
2021-03-08 20:50:47 +0100

    add 'messaging' to contain missing dependencies function

---
+++ messaging/dependencies.py
@@ -0,0 +1,30 @@
+import configparser
+import os
+import sys
+
+from qgis.PyQt.QtWidgets import QMessageBox
+
+
+def report_missing_dependency() -> None:
+    """
+    Reports that one of the dependencies for the project could not be loaded.
+
+    :return: None
+    """
+    python_location = sys.executable
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config = configparser.ConfigParser()
+    config.read(os.path.join(dir_path, 'metadata.txt'))
+    dependencies = config['general']['plugin_dependencies']
+
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText("Unable to load scikit-learn")
+    message = 'You can install with:\n\n' + \
+              '{}'.format(python_location) + \
+              ' -m pip install {}\n\n'.format(dependencies) + \
+              'You can copy-paste this command into a console, and then restart QGIS.'
+
+    msg.setInformativeText(message)
+    msg.setWindowTitle("Error")
+    msg.exec_()

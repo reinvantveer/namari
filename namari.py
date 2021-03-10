04547ef3ea10863587877b3c70b6dfa8883c5071
reinvantveer <reinvantveer@gmail.com>
2021-03-10 21:49:15 +0100

    add type hint to initializer and translator functions, inline verbose, ambiguous and superfluous bootstrapped 'add_action' method

--- namari.py
+++ namari.py
@@ -26,6 +26,7 @@ import os.path
 from qgis.PyQt.QtCore import QCoreApplication, Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
+from qgis.gui import QgisInterface
 from qgis.core import QgsMapLayerProxyModel
 
 # Import the code for the DockWidget
@@ -40,13 +41,13 @@ qInitResources()
 class Namari:
     """QGIS Plugin Implementation."""
 
-    def __init__(self, iface) -> None:
+    def __init__(self, iface: QgisInterface) -> None:
         """Constructor.
 
         :param iface: An interface instance that will be passed to this class
             which provides the hook by which you can manipulate the QGIS
             application at run time.
-        :type iface: QgsInterface
+        :type iface: QgisInterface
         """
 
         # Save reference to the QGIS interface
@@ -66,102 +67,37 @@ class Namari:
         self.dockwidget = None
 
     # noinspection PyMethodMayBeStatic
-    def tr(self, message):
+    def tr(self, message: str) -> str:
         """Get the translation for a string using Qt translation API.
 
         We implement this ourselves since we do not inherit QObject.
 
         :param message: String for translation.
-        :type message: str, QString
+        :type message: str, QStringQgisInterface
 
         :returns: Translated version of message.
-        :rtype: QString
+        :rtype: str
         """
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('Namari', message)
-
-    def add_action(
-            self,
-            icon_path,
-            text,
-            callback,
-            enabled_flag=True,
-            add_to_menu=True,
-            add_to_toolbar=True,
-            status_tip=None,
-            whats_this=None,
-            parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToVectorMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
 
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
         icon_path = ':/plugins/namari/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Detect anomalies'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        text = self.tr(u'Namari anomaly detector')
+        parent = self.iface.mainWindow()
+        icon = QIcon(icon_path)
+
+        action = QAction(icon, text, parent)
+        action.triggered.connect(self.run)
+        action.setEnabled(True)
+        action.setStatusTip("Enables the Namari anomaly detector")
+        action.setWhatsThis("Namari anomaly detector")
+
+        self.toolbar.addAction(action)
+        self.iface.addPluginToVectorMenu(self.menu, action)
+        self.actions.append(action)
 
     # --------------------------------------------------------------------------
 

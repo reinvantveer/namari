61f7e421d7e0671e77c8ae0a5e0e62faf6723acb
reinvantveer <reinvantveer@gmail.com>
2021-04-13 19:50:51 +0200

    add leftovers from previous test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+"""DockWidget test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'rein@vantveer.me'
+__date__ = '2021-02-19'
+__copyright__ = 'Copyright 2021, Rein van \'t Veer'
+
 import unittest
 
 from qgis import utils

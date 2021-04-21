daed9f37ea2c7756664d4f6e76434789be592a16
reinvantveer <reinvantveer@gmail.com>
2021-04-21 13:38:18 +0200

    re-assign NULL string values to ""
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- models/feature_extraction.py
+++ models/feature_extraction.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from PyQt5.QtCore import QVariant
 from qgis.PyQt.QtCore import NULL
 from qgis.core import QgsFeature, QgsVectorLayer
 from sklearn.feature_extraction import DictVectorizer
@@ -28,20 +27,19 @@ def features_to_dicts(layer: QgsVectorLayer):
         if not feature.isValid():
             continue
 
-        types = [type(d) for d in feature]
-        if NULL in feature.attributes():
-            # print('NULL')
-            continue
-
-        if QVariant in types:
-            q_variant_idxs = [t_idx for t_idx, t in enumerate(types) if t == QVariant]
-            null_field_name = field_names[q_variant_idxs[0]]
-            print(f'Skipped feature with fid {feature.id()}: '
-                  f'field {null_field_name} is {feature.attribute(null_field_name)}')
-            assert feature.attribute(null_field_name) == NULL
-            continue
-
+        add_feature = True
         feat_dict = {field: feature.attribute(field) for field in field_names}
-        feat_dicts.append(feat_dict)
+
+        for key, val in feat_dict.items():
+            if val == NULL:
+                if layer.fields().field(key).typeName() == 'String':
+                    feat_dict[key] = ""
+                else:
+                    print(f'Skipped feature with fid {feature.id()}: '
+                          f'field {field_names[key]} is {feature.attribute(key)}')
+                    add_feature = False
+
+        if add_feature:
+            feat_dicts.append(feat_dict)
 
     return feat_dicts
